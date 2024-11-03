46eb984d93101b6cad74583c717162dbeaf4614f
hazhyni <64722307+hazhyni@users.noreply.github.com>
2024-11-03 13:06:50 +0000

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import datetime
 import random
-import matplotlib as plt
 import altair as alt
 import numpy as np
 import pandas as pd
@@ -14,15 +13,24 @@ st.write(
 # :shopping_bags: Brazilian E-Commerce Public Dataset by Olist
 
 **Welcome to Hazhiyah Yumni's Data Analysis X Dicoding Indonesia!**
-This App will help you to know the business improvement over their history and data
+This App will help you to know business improvement according to the data provided.
 """
+)
+
+# Dashboard sidebar
+st.title('Dashboard')
+add_selectbox = st.sidebar.selectbox(
+    'Which Question In Your Mind?',
+    ('Sales Trend', 'Top Sales', 'Most Used Payment',
+     'Delivery Services', 'Negative Feedback Checker')
 )
 
 # Show section to view and edit existing tickets in a table.
 st.header("Sales Trend Over Time")
-st.write(f"Nama Tabel: `orders.csv`")
-st.info("Let's check this out!")
-
+st.write(f"Data Required: `orders.csv`, `order_items.csv`, `order_payments.csv`, `order_reviews.csv`, `products.csv`, `sellers.csv`, `customers.csv`, `geolocation")
+st.info(
+    "As a analyst, We need to know more about sales trends and their total revenue to make the right decisions.",
+    "Here's our clean and structured data to analyze.")
 
 def load_data(url):
     df = pd.read_csv(url)  # 👈 Download the data
@@ -36,3 +44,16 @@ st.dataframe(df)
 # Set the title of the app
 st.title('Matplotlib with Streamlit Example')
 
+# Generate data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create a Matplotlib figure
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title('Sine Wave')
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+
+# Display the plot in Streamlit
+st.pyplot(fig)
